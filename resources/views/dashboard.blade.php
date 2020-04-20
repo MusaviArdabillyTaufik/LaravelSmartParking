9d35f657e868702f60af2f77892f3cf9ede2c2b3
MusaviArdabillyTaufik <201musavi@gmail.com>
2020-04-20 20:10:46 +0700

    dashboard view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -18,42 +18,74 @@
   @endif -->
   <div class="row">
     <div class="col-12 col-md-6 col-lg-4">
-      <div class="card card-secondary">
+      <a href="/admin" class="nounderline">
+      <div class="card card-primary">
         <div class="card-header">
-          <i id="micon" class="fa fa-user-secret" aria-hidden="true"></i>
+            <i id="micon" class="fa fa-user-secret" aria-hidden="true"></i>
           <div class="ml-auto">
             <h4>Total Admin</h4>
             <h3>{{ $totaladmin }}</h3>
           </div>
         </div>
       </div>
+      </a>
     </div>
-
     <div class="col-12 col-md-6 col-lg-4">
+      <a href="/petugas" class="nounderline">
       <div class="card card-secondary">
         <div class="card-header">
-          <i id="micon" class="fa fa-user" aria-hidden="true"></i>
+            <i id="micon" class="fa fa-user" aria-hidden="true"></i>
           <div class="ml-auto">
             <h4>Total Patugas</h4>
             <h3>{{ $totalpetugas }}</h3>
           </div>
         </div>
       </div>
+      </a>
     </div>
-
     <div class="col-12 col-md-6 col-lg-4">
-      <div class="card card-secondary">
+      <a href="/mahasiswa" class="nounderline">
+      <div class="card card-info">
         <div class="card-header">
-          <i id="micon" class="fa fa-user-o" aria-hidden="true"></i>
+            <i id="micon" class="fa fa-user-o" aria-hidden="true"></i>
           <div class="ml-auto">
             <h4>Total Mahasiswa</h4>
             <h3>{{ $totalmahasiswa }}</h3>
           </div>
         </div>
       </div>
+      </a>
     </div>
   </div>
-  <div id="clock" style="font-size: 50px; padding-top: 20%;" align="center"></div>
+  <div class="row d-flex justify-content-around">
+    <div class="col-12 col-md-6 col-lg-3">
+      <a href="/laporan" class="nounderline">
+      <div class="card card-success">
+        <div class="card-header">
+            <i id="micon" class="fa fa-sign-in" aria-hidden="true"></i>
+          <div class="ml-auto">
+            <h4>Total Kendaraan Masuk</h4>
+            <h3 class="text-center">{{ $totalmahasiswa }}</h3>
+          </div>
+        </div>
+      </div>
+      </a>
+    </div>
+    <div class="col-12 col-md-6 col-lg-3">
+      <a href="/laporan" class="nounderline">
+      <div class="card card-danger">
+        <div class="card-header">
+            <i id="micon" class="fa fa-sign-out" aria-hidden="true"></i>
+          <div class="ml-auto">
+            <h4>Total Kendaraan Keluar</h4>
+            <h3 class="text-center">{{ $totalmahasiswa }}</h3>
+          </div>
+        </div>
+      </div>
+      </a>
+    </div>
+  </div>
+  <div id="clock" style="font-size: 50px; padding-top: 10%;" align="center"></div>
   <div id="shadow" align="center" class="d-none"></div>
 		
 </section>
