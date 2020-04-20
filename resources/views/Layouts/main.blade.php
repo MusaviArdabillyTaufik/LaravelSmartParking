78bacb27d0ebac5078c4344a5279539199d2ecc7
MusaviArdabillyTaufik <201musavi@gmail.com>
2020-04-20 18:34:11 +0700

    main layout (for login)

--- resources/views/Layouts/main.blade.php
+++ resources/views/Layouts/main.blade.php
@@ -11,6 +11,7 @@
 
   <!-- Online CSS Files -->
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@500&display=swap" rel="stylesheet">
 
   <!-- Offline CSS -->
   <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
