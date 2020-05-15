84450763f6873c69a4c82267116b6559dff01dd9
MusaviArdabillyTaufik <201musavi@gmail.com>
2020-05-15 11:03:19 +0700

    ok

---
+++ Smart-Parking-master/README.md
@@ -0,0 +1,29 @@
+<p align="center"><img src="https://raw.githubusercontent.com/bagasfa/Smart-Parking/master/public/assets/img/logo.png" width="200"></p>
+
+<h1 align="center">Smart Parking</h1>
+
+<h2>Cara Penggunaan</h2>
+	<ul>
+		<li>Download repo ini ke local anda.</li>
+		<li>Download Composer terlebih dahulu disini <a href="https://getcomposer.org/Composer-Setup.exe">Composer</a></li>
+		<li>Lakukan penginstalan Composer</li>
+		<li>Buatlah database <b>smart_parking</b> pada localhost anda.</li>
+		<li>Extract repo yang telah anda download tadi ke dalam folder <b>xampp/htdocs/-->(disini)<--</b></li>
+		<li>Lalu rename folder <b>Smart-Parking-master</b> menjadi <b>Smart-Parking</b></li>
+		<li>Rename file <b>.env.example</b> menjadi <b>.env</b> yang berada di dalam directory Smart-Parking</li>
+		<li>Buka command prompt yang mengarah ke directory Smart-Parking.</li>
+		<li>Jalankan perintah berikut :
+			<ul>
+				<li>composer install</li>
+				<li>php artisan migrate</li>
+				<li>php artisan db:seed</li>
+				<li>php artisan serve</li>
+			</ul>
+		</li>
+		<li>Buka browser anda dan ketikan " localhost:8000 " pada url browser anda.</li>
+		<li>Email dan password dapat anda lihat pada database <b>smart_parking</b> > tabel <b>user</b>.
+			<br>(password ter-enkripsi menggunakan metode Hashing bCrypt)</li>
+		<li><u>Email</u> digenerate secara random</li>
+		<li><u>Passwordnya</u> adalah <b>password</b></li>
+		<li>Silahkan login dengan senang hati :)</li>
+	</ul>
---
+++ Smart-Parking-master/resources/views/Admin/edit.blade.php
@@ -0,0 +1,68 @@
+@extends('Layouts.adminmain')
+
+@section('content')
+<script type="text/javascript">
+  document.title="Edit Admin";
+  document.getElementById('admin').classList.add('active');
+</script>
+<section class="section">
+  
+  <div class="section-header">
+    <h1>
+      Admin <small>Edit Data</small>
+    </h1>
+  </div>
+
+  <div class="section-body">
+    <div class="col-12 col-md-6 col-lg-6">
+        <div class="card">
+          <div class="card-header">
+            <a href="{{ url('/admin') }}"> 
+              <button type="button" class="btn btn-outline-danger">
+                <i class="fas fa-arrow-circle-left"></i> Kembali
+              </button>
+          </a>
+          </div>
+          <div class="card-body">
+            <form action="{{ url('/admin/'.$user->id.'/update') }}" method="POST">
+              @csrf
+              <div class="form-group">
+                  <label for="inputNIK">NIK</label>
+                  <input name="nik" type="text" pattern="\d*" maxlength="16" class="form-control" id="inputNIK" placeholder="Nomor Induk Kependudukan" value="{{ $user->nik }}" required="">
+              </div>
+              <div class="form-group">
+                  <label for="inputNama">Nama Lengkap</label>
+                  <input name="nama_user" type="text" class="form-control" id="inputNama" placeholder="Nama Lengkap" value="{{ $user->nama_user }}" required="">
+              </div>
+              <div class="form-group">
+                  <label for="inputEmail">E-mail</label>
+                  <input name="email" type="email" class="form-control" id="inputEmail" placeholder="E-mail" value="{{ $user->email }}" required="">
+              </div>
+              <div class="form-group">
+                  <label for="inputPassword">Password <i style="color: red;">*</i></label>
+                  <div class="input-group" id="show_hide_password">
+                    <input name="password" type="password" minlength="8" class="form-control" id="inputPassword" placeholder="Password" value="{{ $user->pass_kotlin }}" required="">
+                    <a href=""><div class="input-group-addon eye">
+                      <i class="fa fa-eye-slash" aria-hidden="true"></i>
+                    </div></a>
+                  </div>
+              </div>
+              <div class="form-group">
+                  <label for="inputTelp">No. Telpon</label>
+                  <input name="telfon" type="tel" maxlength="12" class="form-control" id="inputTelp" placeholder="Nomor Telpon" value="{{ $user->telfon }}" required="">
+              </div>
+              <div class="form-group">
+                  <label for="inputAlamat">Alamat</label>
+                  <textarea class="form-control" name="alamat" placeholder="Alamat Lengkap" rows="6" required="">{{ $user->alamat }}</textarea>
+              </div>
+              <div class="form-group text-right">
+                <button type="submit" class="btn btn-primary">SIMPAN</button>
+              </div>
+              </form>
+          </div>
+        </div>
+      </div>  
+  </div>
+
+</section>
+@endsection
